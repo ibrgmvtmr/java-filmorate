8bb17fe1747d8da0ff025f21cee8666f13a2f95d
Timur <storyphase@yandex.com>
2023-07-07 15:02:15 +0600

    Исправил валидацию даты

--- src/main/java/ru/yandex/practicum/javafilmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controllers/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
         log.info("Добавление нового пользователя: {}", user);
-        if (user.getName() == null|| user.getName().isBlank() || user.getName().isEmpty()) {
+        if (user.getName() == null || user.getName().isBlank() || user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
         user.setId(generatedId++);
