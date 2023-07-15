57667d6adc93b98c0b08a67f06bba1e10f849277
Timur <storyphase@yandex.com>
2023-07-15 17:30:49 +0600

    feat: Добавил методы по просмотру лучших фильмов, лайки для фильмов, методы по добавлению в друзья и также методы по удалению друзей и по просмотру общих друзей

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/interfaceStorage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/interfaceStorage/FilmStorage.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.javafilmorate.storage.interfaceStorage;
 
-import org.springframework.context.annotation.Primary;
 import ru.yandex.practicum.javafilmorate.model.Film;
 
 import java.util.List;
-@Primary
+
 public interface FilmStorage {
+
     List<Film> getFilms();
 
     Film createFilm(Film film);
