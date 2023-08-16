0b7baee7ff355ddfb0becfd951fa7339cdd9e521
Timur <storyphase@yandex.com>
2023-08-16 15:39:35 +0600

    feat: Добавил обработку данных для всех классов, Добавил сервисные классы для рейтинга и жанров

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/dbimpl/user/FriendshipDbStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dbimpl/user/FriendshipDbStorage.java
@@ -12,9 +12,9 @@ import java.util.List;
 @Component
 public class FriendshipDbStorage implements FriendshipStorage {
 
-    @Autowired
     private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public FriendshipDbStorage(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -22,8 +22,8 @@ public class FriendshipDbStorage implements FriendshipStorage {
     @Override
     public void addFriend(Integer userId, Integer friendId) {
         String sqlQuery = "INSERT INTO FRIENDSHIPS\n" +
-                          "(USER_ID, FRIEND_ID)\n" +
-                          "VALUES(?,?)";
+                "(USER_ID, FRIEND_ID)\n" +
+                "VALUES(?, ?)";
         jdbcTemplate.update(sqlQuery, userId, friendId);
     }
 
@@ -35,7 +35,7 @@ public class FriendshipDbStorage implements FriendshipStorage {
                 "    SELECT FRIEND_ID\n" +
                 "    FROM FRIENDSHIPS\n" +
                 "    WHERE USERS.USER_ID = ?\n" +
-                ");";
+                ")";
         return jdbcTemplate.query(sqlQuery, new BeanPropertyRowMapper<>(User.class), userId);
     }
 
@@ -45,14 +45,14 @@ public class FriendshipDbStorage implements FriendshipStorage {
                 "FROM USERS u\n" +
                 "JOIN FRIENDSHIPS f1 ON u.user_id = f1.friend_id\n" +
                 "JOIN FRIENDSHIPS f2 ON f1.user_id = f2.friend_id\n" +
-                "WHERE f1.user_id = ? AND f2.user_id = ?;";
+                "WHERE f1.user_id = ? AND f2.user_id = ?";
         return jdbcTemplate.query(sqlQuery, new BeanPropertyRowMapper<>(User.class), userId, friendId);
     }
 
     @Override
     public void removeFriend(Integer userId, Integer friendId) {
         String sql = "DELETE FROM FRIENDSHIPS\n" +
-                "WHERE FRIEND_ID=? AND USER_ID=?;\n";
+                "WHERE FRIEND_ID = ? AND USER_ID = ?";
         jdbcTemplate.update(sql, friendId, userId);
     }
 }
