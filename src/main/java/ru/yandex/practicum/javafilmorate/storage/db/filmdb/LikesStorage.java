28c06ad95c990b0ec266d838048c1c97572c5aaa
Timur <storyphase@yandex.com>
2023-08-16 15:34:29 +0600

    feat: Добавил обработку данных для всех классов, Добавил сервисные классы для рейтинга и жанров

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/db/filmdb/LikesStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/db/filmdb/LikesStorage.java
@@ -4,7 +4,9 @@ import java.util.Set;
 
 public interface LikesStorage {
     void addLike(Integer userId, Integer filmId);
+
     Set<Integer> readUsersLikes(Integer filmId);
+
     void deleteLike(long userId, long filmId);
 
 }
