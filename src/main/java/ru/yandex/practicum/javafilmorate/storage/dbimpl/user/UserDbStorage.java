a9c49df7dd19fadea439466def80b348259e7059
Timur <storyphase@yandex.com>
2023-08-17 19:53:23 +0600

    feat: Добавил обработку данных для всех классов, Добавил сервисные классы для рейтинга и жанров

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/dbimpl/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dbimpl/user/UserDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.javafilmorate.storage.dbimpl.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
