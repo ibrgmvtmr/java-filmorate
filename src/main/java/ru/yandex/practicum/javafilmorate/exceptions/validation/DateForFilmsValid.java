2d3847f71c928624dd19d62740ad09a313ec5338
Timur <storyphase@yandex.com>
2023-08-20 21:32:33 +0600

    feat: Добавил обработку данных для всех классов, Добавил сервисные классы для рейтинга и жанров

---
+++ src/main/java/ru/yandex/practicum/javafilmorate/exceptions/validation/DateForFilmsValid.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.javafilmorate.exceptions.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class DateForFilmsValid implements ConstraintValidator<DateForFilms, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext constraintValidatorContext) {
+        return releaseDate.isAfter(LocalDate.of(1895, 12, 28));
+    }
+}
